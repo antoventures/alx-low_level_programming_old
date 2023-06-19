6919721bb183d388252341240ef7ef7cef7828da
antoventures <antoventures@gmail.com>
2023-06-19 10:18:48 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+char c;
+char d;
+for(c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+for (d = 'A'; d<= 'Z' d++)
+{
+putchar(d);
+}
+return (0);
+}
