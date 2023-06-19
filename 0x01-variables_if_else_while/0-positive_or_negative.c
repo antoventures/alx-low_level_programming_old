678937eccd6c7b4f3696a79a151af717997f5dcb
antoventures <antoventures@gmail.com>
2023-06-19 09:12:46 -0700

    0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 {
-prinf("%d: is positive"n);
+prinf("%d: is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%d: is zero"n);
+printf("%d: is zero\n", n);
 }
 else
 {
-printf("%d: is netagive"n);
+printf("%d: is netagive\n", n);
 }
 return (0);
 }
