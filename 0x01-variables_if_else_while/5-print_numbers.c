5fe0782d267acf20e51307ebb04f43d40b8a9f43
antoventures <antoventures@gmail.com>
2023-06-20 08:14:51 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - printing  Numbers
+ * Return: 0 for success
+ */
+int main(void)
+{
+int count;
+while (count = 0 && count < 10)
+{
+	putchar(count);
+	count++;
+}
+putchar('\n');
+return (0);
+}
