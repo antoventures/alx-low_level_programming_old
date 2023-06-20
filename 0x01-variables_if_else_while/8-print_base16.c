e3a589cb708f9f2d20115c0103d98acbde345eea
antoventures <antoventures@gmail.com>
2023-06-20 09:46:05 -0700

    8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,10 +11,10 @@ int i, a;
 for (i = 0; i < 10; i++)
 {
 putchar(i);
-	for (a = 'a'; a <= 'f'; a++)
-	{
-		putchar(a);
-	}
+}
+for (a = 'a'; a <= 'f'; a++)
+{
+putchar(a);
 }
 putchar('\n');
 return (0);
