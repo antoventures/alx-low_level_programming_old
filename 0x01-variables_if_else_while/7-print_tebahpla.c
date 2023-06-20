6d264fdf6465d624c919fb3b6aafc487f3f51750
antoventures <antoventures@gmail.com>
2023-06-20 09:17:28 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in reverse order
+ * Return: 0 for success
+ */
+int main(void)
+{
+char z;
+z = 'a';
+z = 'z';
+for ( z = 'z'; z >= 'a'; z--)
+       putchar(z);
+putchar('\n');
+}
