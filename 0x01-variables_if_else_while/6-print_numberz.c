415c9487e72880fc7f4331a4c142210c75d09ce0
antoventures <antoventures@gmail.com>
2023-06-20 08:27:14 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print single digit numbers
+ * Return: 0 for success
+ */
+int main(void)
+{
+int i;
+i = 0;
+while (i < 10)
+{
+	putchar(i, '\n');
+}
+return (0);
+}
