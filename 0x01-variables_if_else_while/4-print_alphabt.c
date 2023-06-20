5295e520ec4a2c308c58fe94ba563f30f2905e2d
antoventures <antoventures@gmail.com>
2023-06-20 07:27:43 -0700

    4. When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print all the letters except q and e
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	char c, e, q;
+	c = e = '';
+	c = q = '';
+
+	for (c = 'a'; c <= 'z'; c++;
+		{
+		putchar (c);
+		}
+putchar ('\n');
+return (0);
+}
