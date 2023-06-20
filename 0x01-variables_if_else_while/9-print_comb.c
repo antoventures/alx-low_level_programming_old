b6413b4531d7cfb76dc535b0da2ad69b33b29e12
antoventures <antoventures@gmail.com>
2023-06-20 10:08:16 -0700

    combinations of single-digit numbers.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - combinations of single-digit numbers.
+ * Return: 0 success
+ */
+
+int main(void)
+{
+int i;
+i = 0;
+while (i <= 9)
+	putchar(i + ',');
+	i++;
+return (0);
+}
